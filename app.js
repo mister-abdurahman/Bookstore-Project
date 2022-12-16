7f5f4fc2d64ade2132fbea0039e5d00f0f104048
Aramide Ramadan <92337603+mister-abdurahman@users.noreply.github.com>
2022-12-16 18:33:08 +0100

    Initialize Project and book route

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const CONFIG = require('./config/config')
+const connectToDB = require('./db/mongodb')
+
+// Routers
+const bookRouter = require('./routes/books')
+
+const app = express()
+
+// connect to mongoDB
+connectToDB()
+
+//Middlewares
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use('/api/v1/books', bookRouter)
+
+app.get('/', (req, res) => {
+    res.send("Hello Bookstore")
+})
+
+// Error handler midddleware
+app.use((err, req, res, next) => {
+    console.log(err)
+
+    const errorStatus = err.status || 500
+
+    res.status(errorStatus).send(err.message)
+    next()
+})
+
+app.listen(CONFIG.PORT, () => {
+    console.log(`Server started on http://localhost:${CONFIG.PORT}`)
+})
